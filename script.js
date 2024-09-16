8874cef3b7abf28d490d84ffe32a5b6dca45f55f
JmzTradehacker <japhet.mdn@gmail.com>
2024-09-15 20:18:19 -0700

    added Prime time logic

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Fizz Buzz
 for (let i = 1; i <= 100; i++) {
   if (i % 15 === 0) {
     console.log("Fizz Buzz");
@@ -7,5 +8,28 @@ for (let i = 1; i <= 100; i++) {
     console.log("Buzz");
   } else {
     console.log(i);
+  }
+}
+
+//Prime Time
+
+let n = 9;
+let number = n + 1;
+
+while (true) {
+  let isPrime = true;
+
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) {
+      isPrime = false;
+      break;
+    }
+  }
+
+  if (isPrime) {
+    console.log("The next prime number after", n, "is", number);
+    break;
+  } else {
+    number++;
   }
 }
